f907eea5be946acaa4673664af40e0c69e4f897b
Drew Crampsie <me@drewc.ca>
2024-01-22 22:23:38 -0800

    Totally new updates

---
+++ early-init.el
@@ -0,0 +1,13 @@
+;; The Early Init
+;; :PROPERTIES:
+;; :CUSTOM_ID: the-early-init
+;; :END:
+
+;; Emacs has/had things that we want to configure BEFORE initialization, as it were.
+
+;; So for that we get the [[info:emacs#Early Init File][Early Init File]](info), AKA [[https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html][early-init.el]](html).
+
+
+;; [[file:README.org::*The Early Init][The Early Init:1]]
+(setq package-enable-at-startup nil)
+;; The Early Init:1 ends here
--- init.el
+++ init.el
@@ -15,85 +15,107 @@
 ;; Set up use-package for tidier package configuration/installation
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
-(straight-use-package 'org)
 
-
-(require 'ox-md)
 (setq ns-command-modifier 'meta)
 (setq  mac-command-modifier 'meta)
 
+(straight-use-package 'org)
+(require 'ox-md) (require 'org-indent)
+(org-babel-do-load-languages
+   'org-babel-load-languages
+   '((sql . t)
+     (shell .t )))
+
+(use-package outli
+  :straight (outli :type git :host github :repo "jdtsmith/outli")
+  :bind (:map outli-mode-map ; convenience key to get back to containing heading
+	      ("C-c C-p" . (lambda () (interactive) (outline-back-to-heading)))))
+
+(use-package ox-gfm)
+(require 'ox-gfm)
+
+(use-package org-make-toc
+  :config
+   (setq org-make-toc-insert-custom-ids t))
+
+(desktop-save-mode 1)
 
 (use-package evil)
 (require 'evil)
 (evil-mode 1)
 
-(progn
-  (defvar *gerbil-path*
-    (or (getenv "GERBIL_INSTALL_PREFIX")
-	(shell-command-to-string "gxi -e '(display (path-expand \"~~\"))'\
+(use-package bnf-mode)
+
+(eval-and-compile
+  (setq *gerbil-path*
+	(or (getenv "GERBIL_INSTALL_PREFIX")
+	    (shell-command-to-string "gxi -e '(display (path-expand \"~~\"))'\
    -e '(flush-output-port)'")))
+  (setq font-lock-reference-face font-lock-constant-face)
+  (defun %gerbil-load-path ()
+     (expand-file-name "share/emacs/site-lisp/" *gerbil-path*)))
 
-  (use-package gerbil-mode
-    :when (file-directory-p *gerbil-path*)
-    :ensure nil
-    :straight nil
-    :defer t
-    :mode (("\\.ss\\'"  . gerbil-mode)
-           ("\\.pkg\\'" . gerbil-mode))
-    :bind (:map comint-mode-map
-		(("C-S-n" . comint-next-input)
-		 ("C-S-p" . comint-previous-input)
-		 ("C-S-l" . clear-comint-buffer))
-		:map gerbil-mode-map
-		(("C-S-l" . clear-comint-buffer)))
-    :init
-    (autoload 'gerbil-mode
-      (expand-file-name "share/emacs/site-lisp/gerbil-mode.el" *gerbil-path*)
-      "Gerbil editing mode." t)
-    :hook
-    ((gerbil-mode-hook . linum-mode)
-     (inferior-scheme-mode-hook . gambit-inferior-mode)
-     (scheme-mode-hook . gerbil-mode))
+(use-package gerbil-mode
+  :when (file-directory-p *gerbil-path*)
+  :load-path (lambda () (list (%gerbil-load-path)))
+  :ensure nil
+  :straight nil
+  ;;:demand t
+ ;; :defer t
+  :mode (("\\.ss\\'"  . gerbil-mode)
+         ("\\.pkg\\'" . gerbil-mode))
+  :bind (:map comint-mode-map
+	      (("C-S-n" . comint-next-input)
+	       ("C-S-p" . comint-previous-input)
+	       ("C-S-l" . clear-comint-buffer))
+	      :map gerbil-mode-map
+	      (("C-S-l" . clear-comint-buffer)))
+  ;; :init
+  ;; (autoload 'gerbil-mode
+  ;;   (expand-file-name "share/emacs/site-lisp/gerbil-mode.el" *gerbil-path*)
+  ;;   "Gerbil editing mode." t)
+   :hook
+   ((gerbil-mode . display-line-numbers-mode)
+    (inferior-scheme-mode . gambit-inferior-mode)
+    (scheme-mode . gerbil-mode))
 
-    :config
-    (require 'gambit
-             (expand-file-name "share/emacs/site-lisp/gambit.el" *gerbil-path*))
-    (setf scheme-program-name (expand-file-name "bin/gxi" *gerbil-path*))
+  :config
+  (require 'gambit
+           (expand-file-name "share/emacs/site-lisp/gambit.el" *gerbil-path*))
+  (setf scheme-program-name (expand-file-name "bin/gxi -:te" *gerbil-path*))
 
-    (let ((tags (locate-dominating-file default-directory "TAGS")))
-      (when tags (visit-tags-table tags)))
-    (let ((tags (expand-file-name "src/TAGS" *gerbil-path*)))
-      (when (file-exists-p tags) (visit-tags-table tags)))
+  (let ((tags (locate-dominating-file default-directory "TAGS")))
+    (when tags (visit-tags-table tags)))
+  (let ((tags (expand-file-name "src/TAGS" *gerbil-path*)))
+    (when (file-exists-p tags) (visit-tags-table tags)))
 
-    (when (package-installed-p 'smartparens)
-      (sp-pair "'" nil :actions :rem)
-      (sp-pair "`" nil :actions :rem))
+  (when (package-installed-p 'smartparens)
+    (sp-pair "'" nil :actions :rem)
+    (sp-pair "`" nil :actions :rem))
 
-    (defun clear-comint-buffer ()
-      (interactive)
-      (with-current-buffer "*scheme*"
-	(let ((comint-buffer-maximum-size 0))
-          (comint-truncate-buffer)))))
-
-  (defun gerbil-setup-buffers ()
-    "Change current buffer mode to gerbil-mode and start a REPL"
+  (defun clear-comint-buffer ()
     (interactive)
-    (gerbil-mode)
-    (split-window-right)
-    (shrink-window-horizontally 2)
-    (let ((buf (buffer-name)))
-      (other-window 1)
-      (run-scheme "gxi")
-      (switch-to-buffer-other-window "*scheme*" nil)
-      (switch-to-buffer buf)))
+    (with-current-buffer "*scheme*"
+      (let ((comint-buffer-maximum-size 0))
+        (comint-truncate-buffer))))
 
-  (global-set-key (kbd "C-c C-g") 'gerbil-setup-buffers))
+)
+
+(defun gerbil-setup-buffers ()
+  "Change current buffer mode to gerbil-mode and start a REPL"
+  (interactive)
+  (gerbil-mode)
+  (split-window-right)
+  (shrink-window-horizontally 2)
+  (let ((buf (buffer-name)))
+    (other-window 1)
+    (run-scheme "gxi")
+    (switch-to-buffer-other-window "*scheme*" nil)
+    (switch-to-buffer buf)))
+
+;; (global-set-key (kbd "C-c C-g") 'gerbil-setup-buffers)
 
 
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((sql . t)))
 (use-package markdown-mode
   :mode ("README\\.md\\'" . gfm-mode))
 
