64b122517e967a3fdcdf06e20da66abb3dc2ec5c
Drew Crampsie <me@drewc.ca>
2024-02-18 21:48:16 -0800

    New roam/ directory

---
+++ roam/00-org-roam.el
@@ -0,0 +1,22 @@
+(use-package org-roam
+  :straight t
+  :custom
+  (org-roam-directory (file-truename "~/me/braindump/roam"))
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n g" . org-roam-graph)
+	 ("C-c n i" . org-roam-node-insert)
+	 ("C-c n c" . org-roam-capture)
+	 ;; Dailies
+	 ("C-c n j" . org-roam-dailies-capture-today)
+	 :map org-mode-map
+	 ("C-M-i" . completion-at-point)
+	 :map minibuffer-local-completion-map
+	 ("SPC" . self-insert-command))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
